a769516380c7044fdb7e8fdfabf1bc3364bdec67
kris5179 <krzys.nazarko@gmail.com>
2026-03-04 19:54:40 +0100

    pierwszy commit, luźne próby z pythonem wieczorkiem

---
+++ main.py
@@ -0,0 +1,21 @@
+testowy_tekst = "AbCdefYZZ1234##$"
+
+print("a = " + str(ord("a")))
+print("A = " + str(ord("A")))
+print("z = " + str(ord("z")))
+print("Z = " + str(ord("Z")))
+
+def encode(tekst):
+    lista = list(tekst)
+    for i in range(len(lista)):
+        znak = ord(lista[i])
+        znak = znak + 1
+
+        if znak == 90:
+            znak = 65
+        lista[i] = chr(znak)
+    return ''.join(lista)
+
+
+print(testowy_tekst)
+print(encode(testowy_tekst))
